8d656f2cd05566c912511ef20dfdfee050b25870
amarphule <amardeepphule@gmail.com>
2023-09-16 17:27:58 +0530

    refact: removed console.logs/comments and warnings

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,22 +5,11 @@ import { fetchProducts } from "../../slices/productsSlice";
 import Shimmer from "../Card/Shimmer";
 
 const Home = () => {
-  const dispatch = useDispatch();
-
   const { products, isLoading, error } = useSelector((store) => store.products);
-
-  console.log({ products, isLoading, error });
+  const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchProducts());
   }, []);
-
-  //   if (isLoading) {
-  //     return (
-  //       <div className="flex flex-wrap justify-center">
-  //         <Shimmer />;
-  //       </div>
-  //     );
-  //   }
 
   if (error) {
     return <h1 className="text-center text-red-600">{error}</h1>;
